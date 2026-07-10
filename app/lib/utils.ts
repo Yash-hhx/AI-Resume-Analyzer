fa9701e8b315be21eaed771bbee9901327b64705
Yash-hhx <yashsinha852@gmail.com>
2026-07-11 01:38:12 +0530

    Resume Detailed Feedback Components

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -1,4 +1,9 @@
-import FileUploader from "~/components/FileUploader";
+import clsx, {type ClassValue} from "clsx";
+import {twMerge} from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+    return twMerge(clsx(inputs))
+}
 
 export function formatSize(bytes: number): string {
     if (bytes === 0) return "0 KB";
